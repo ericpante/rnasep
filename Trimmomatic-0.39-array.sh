3117790b04a8a4c48903d5d48ab8bfab36a09d86
TSolDour <70651871+TSolDour@users.noreply.github.com>
2024-07-05 10:36:25 +0200

    Add files via upload
    Trimmomatic script and config files used for cleaning raw data.

---
+++ Trimmomatic-0.39-array.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p workq
+#SBATCH --job-name=TrimArrayJob
+#SBATCH -t 02:00:00 #Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=6
+#SBATCH --array=1-36
+
+#Load modules
+module load bioinfo/Trimmomatic/0.39
+
+cd /home/epante/work/rnaseq/2-Trimming/.
+config=Trimmomatic-Config.txt
+READ1=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print$2}' $config)
+READ2=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print$3}' $config)
+OUTPUT=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print$4}' $config)
+
+java -jar $TRIM_HOME/trimmomatic.jar PE -threads 4 -phred33 ${READ1} ${READ2} ${OUTPUT}_R1_pe.fastq.gz ${OUTPUT}_R1_se.fastq.gz ${OUTPUT}_R2_pe.fastq.gz ${OUTPUT}_R2_se.fastq.gz ILLUMINACLIP:$TRIM_HOME/adapters/TruSeq3-PE.fa:2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:100
