44b590d36d31a216a2ede82750dc277f5095e744
TSolDour <70651871+TSolDour@users.noreply.github.com>
2024-07-11 10:41:21 +0200

    Update Trimmomatic-0.39-array.sh

--- Trimmomatic-0.39-array.sh
+++ Trimmomatic-0.39-array.sh
@@ -15,4 +15,4 @@ READ1=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print$2}' $con
 READ2=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print$3}' $config)
 OUTPUT=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print$4}' $config)
 
-java -jar $TRIM_HOME/trimmomatic.jar PE -threads 4 -phred33 ${READ1} ${READ2} ${OUTPUT}_R1_pe.fastq.gz ${OUTPUT}_R1_se.fastq.gz ${OUTPUT}_R2_pe.fastq.gz ${OUTPUT}_R2_se.fastq.gz ILLUMINACLIP:$TRIM_HOME/adapters/TruSeq3-PE.fa:2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:100
+java -jar $TRIM_HOME/trimmomatic.jar PE -threads 4 -phred33 ${READ1} ${READ2} ${OUTPUT}_R1_pe.fastq.gz ${OUTPUT}_R1_se.fastq.gz ${OUTPUT}_R2_pe.fastq.gz ${OUTPUT}_R2_se.fastq.gz ILLUMINACLIP:$TRIM_HOME/adapters/TruSeq3-PE.fa:2:30:10 LEADING:20 TRAILING:20 HEADCROP:10 SLIDINGWINDOW:4:20 MINLEN:100
