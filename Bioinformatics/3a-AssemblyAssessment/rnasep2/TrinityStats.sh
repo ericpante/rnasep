22db57982b83ee45136e7dca86c52d209853ed25
Thomas Sol Dourdin <t.soldourdin@gmail.com>
2025-03-18 14:33:24 +0100

    Scripts used for Assembly Quality Assessment.

---
+++ Bioinformatics/3a-AssemblyAssessment/rnasep2/TrinityStats.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH -J Trinity_Stats
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=10G
+#SBATCH -t 05:00:00
+#SBATCH --error=trinity_%j.err
+#SBATCH --output=trinity_%j.out
+
+#Purge unused modules
+module purge
+
+#Load modules
+module load trinity/2.15.1
+
+cd /shared/projects/rnasep/3a-AssemblyAssessment/
+
+TrinityStats.pl Trinity_rnasep2.Trinity.fasta
