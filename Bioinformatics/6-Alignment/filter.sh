427107b35b9634be4938927dc862f8cdc2f0b6f9
Thomas Sol Dourdin <t.soldourdin@gmail.com>
2025-04-08 10:58:17 +0200

    Updating the 'Alignment' folder.'

--- Bioinformatics/6-Alignment/filter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in *.tab.tmp
-do
-    LANG=en_EN sort -k1,1 ${i} > ${i}.tmp
-    LANG=en_EN join -1 1 -2 1 mRNA.lst ${i}.tmp | sed 's/ /\t/g' | cut -f1-2 > ${i%Reads*}.counts
-done
