f77050d4fd3ad68cd526c314e0601832d62cd5cc
Thomas Sol Dourdin <t.soldourdin@gmail.com>
2025-05-19 17:12:59 +0200

    nxt

---
+++ Bioinformatics/4-AssemblyAssessment/rnasep1/Assembly_Thin/EXN50/02-Kallisto-Quant-array.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -p fast
+#SBATCH -J Quant_Array
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=20G
+#SBATCH -t 03:00:00
+#SBATCH --error=Quant_Array%j.err
+#SBATCH --output=Quant_Array%j.out
+#SBATCH --array=1-16
+
+#Purge unused modules
+module purge
+
+#Load modules
+module load kallisto/0.46.2
+
+#Defining variables
+config=Kallisto-Config.txt
+INDEX=rnasep1_Trinity95_Kallisto
+READ1=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print$2}' $config)
+READ2=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print$3}' $config)
+OUTPUT=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print$4}' $config)
+
+
+cd /shared/projects/rnasep/4-AssemblyAssessment/rnasep1/Assembly_Thin/ExN90/
+
+
+
+kallisto quant -i ${INDEX} -o Abundance_${OUTPUT} ${READ1} ${READ2}
